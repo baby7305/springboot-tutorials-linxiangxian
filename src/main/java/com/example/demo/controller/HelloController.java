4fbe643b187e3c6188ef039ed47dd587a62b896d
baby7305 <songhaibei7305@163.com>
2019-04-07 17:34:13 +0800

    新建实体，返回实体的json数据

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.bean.Demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,13 @@ public class HelloController {
 	@RequestMapping("/hello")
 	public String Hello() {
 		return "hello baby";
+	}
+
+	@RequestMapping("/getDemo")
+	public Demo getDemo() {
+		Demo demo = new Demo();
+		demo.setId(1);
+		demo.setName("张三");
+		return demo;
 	}
 }
