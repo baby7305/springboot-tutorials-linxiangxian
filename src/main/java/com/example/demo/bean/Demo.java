e687efd9f3bb73055b574d9e2cb590184e868f16
baby7305 <songhaibei7305@163.com>
2019-04-07 18:25:43 +0800

    序列化时间字段，过滤字段到json

--- src/main/java/com/example/demo/bean/Demo.java
+++ src/main/java/com/example/demo/bean/Demo.java
@@ -1,8 +1,32 @@
 package com.example.demo.bean;
 
+import com.alibaba.fastjson.annotation.JSONField;
+
+import java.util.Date;
+
 public class Demo {
 	private int id;
 	private String name;
+	@JSONField(format="yyyy-MM-dd HH:mm")
+	private Date createtime;
+	@JSONField(serialize=false)
+	private String remarks;
+
+	public Date getCreatetime() {
+		return createtime;
+	}
+
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
 
 	public int getId() {
 		return id;
--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -4,6 +4,8 @@ import com.example.demo.bean.Demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class HelloController {
 	@RequestMapping("/hello")
@@ -16,6 +18,8 @@ public class HelloController {
 		Demo demo = new Demo();
 		demo.setId(1);
 		demo.setName("张三");
+		demo.setCreatetime(new Date());
+		demo.setRemarks("这是备注信息");
 		return demo;
 	}
 }
